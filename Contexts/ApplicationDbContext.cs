2a263ed4847ce36a4ab030c53dcf7ff905e1a786
Felipe Tapias <atapias@sistecredito.com>
2023-01-03 15:11:14 -0500

    Se crea todo el CRUD para los autores

---
+++ Contexts/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiPrimerWebAPIM3.Entities;
+
+namespace MiPrimerWebAPIM3.Contexts
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+
+    }
+}
